758200fa1c8dfffb4b9d72cea24b842cfb307c35
Piotr Nowek <piotrek21125@wp.pl>
2024-08-26 14:05:09 +0200

    Added inventory_page and refactored test_login to use POM

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,4 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.options import Options
 import pytest 
@@ -16,25 +15,4 @@ def setup():
     driver.get(url)
     driver.implicitly_wait(5)  
     yield driver
-    driver.quit()
-
-
-def login(driver):
-    """Login to system with valid data"""
-    username_input = driver.find_element(By.ID, "user-name")
-    password_input = driver.find_element(By.ID,"password")
-    login_button = driver.find_element(By.ID,"login-button")
-
-    username_input.send_keys("standard_user")
-    password_input.send_keys("secret_sauce")
-    login_button.click()
-    
-
-def logout(driver):
-    """Logout and close browser"""
-    sidebar_button = driver.find_element(By.ID, "react-burger-menu-btn")
-    logout_button = driver.find_element(By.ID, "logout_sidebar_link")   
-    sidebar_button.click()
-    logout_button.click()
-    driver.quit()
-   
+    driver.quit()  
