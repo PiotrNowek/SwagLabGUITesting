455fff8f200ad4b479aa74b30b85124a0a2fdcc0
Piotr Nowek <piotrek21125@wp.pl>
2024-08-26 12:45:03 +0200

    Added firefox driver path to config

--- tests/config/config.py
+++ tests/config/config.py
@@ -1,2 +1,2 @@
-driver_path = 'D:/SwagLabGUITesting/drivers/geckodriver.exe'
+firefox_driver_path = 'D:/SwagLabGUITesting/drivers/geckodriver.exe'
 url = 'https://www.saucedemo.com/'
--- tests/conftest.py
+++ tests/conftest.py
@@ -4,14 +4,14 @@ from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.options import Options
 import pytest 
 
-from config.config import driver_path, url
+from config.config import firefox_driver_path, url
 
 
 @pytest.fixture
 def setup():
     """Setup for each test case"""
     options = Options()
-    service = Service(driver_path)
+    service = Service(firefox_driver_path)
     driver = webdriver.Firefox(service=service, options=options)
     driver.get(url)
     driver.implicitly_wait(5)  
